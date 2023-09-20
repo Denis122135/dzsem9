fa655b44ae547a2b5309c924fed0e3e8c25bb1ae
Denis <www.00.www38@gmail.com>
2023-09-20 19:42:19 +0600

    o21

--- Program.cs
+++ Program.cs
@@ -52,19 +52,19 @@
 
 
 
-int FunctionAccerman (int m, int n)
-{
-    if (m == 0)
-        return n + 1;
-    else if (n == 0)
-        return FunctionAccerman(m - 1, 1);
-    return FunctionAccerman(m - 1, FunctionAccerman(m, n -1));
-}
+// int FunctionAccerman (int m, int n)
+// {
+//     if (m == 0)
+//         return n + 1;
+//     else if (n == 0)
+//         return FunctionAccerman(m - 1, 1);
+//     return FunctionAccerman(m - 1, FunctionAccerman(m, n -1));
+// }
 
 
-Console.Clear();
-Console.WriteLine("Введите число M");
-int m = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число N");
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Итог: {FunctionAccerman(m, n)}");
+// Console.Clear();
+// Console.WriteLine("Введите число M");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число N");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"Итог: {FunctionAccerman(m, n)}");
